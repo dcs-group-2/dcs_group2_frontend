1aac1fba94dfce1a2e6987fcc6a688e474bf2f3b
kper <krper@gmx.at>
2025-03-12 15:39:57 +0100

    fix: change redirectURI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   msal: {
     clientId: '1d5d790f-f1c2-41bc-80df-e57e3642b219',
     authority: 'https://login.microsoftonline.com/4d6aeeaa-63b4-4dd7-90c7-28ddde1daea4',
-    redirectUri: 'https://attendance.sueniversity.nl',
+    redirectUri: 'https://attendance.sueniversity.nl/login',
   },
   appInsights: {
     instrumentationKey: 'd0e978bf-a8f7-4e85-9a7d-05fc97e3b1dd'

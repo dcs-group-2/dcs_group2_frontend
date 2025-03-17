df62eb6de06674a97c139e4969b703a33eb0db41
kper <krper@gmx.at>
2025-03-17 15:24:10 +0100

    fix: Make SSO work locally

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   msal: {
     clientId: '1d5d790f-f1c2-41bc-80df-e57e3642b219',
     authority: 'https://login.microsoftonline.com/4d6aeeaa-63b4-4dd7-90c7-28ddde1daea4',
-    redirectUri: 'http://localhost:4200',
+    redirectUri: 'http://localhost:4200/login',
   },
   appInsights: {
     instrumentationKey: 'd0e978bf-a8f7-4e85-9a7d-05fc97e3b1dd'
